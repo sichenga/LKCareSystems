73d16e31898805bbcc2923f0e971db5eb38b5b21
xujingxin <3181959755@qq.com>
2024-06-26 17:19:16 +0800

    feat: :sparkles: 增加账号模块

--- src/components/dialog/system/ManagementDialog.vue
+++ src/components/dialog/system/ManagementDialog.vue
@@ -7,69 +7,90 @@
   >
     <el-form
       ref="ruleFormRef"
-      style="max-width: 600px"
       :model="ruleForm"
       :rules="rules"
-      label-width="auto"
-      class="demo-ruleForm"
       :size="formSize"
+      class="demo-ruleForm"
+      label-width="auto"
       status-icon
+      style="max-width: 600px"
     >
+      <el-form-item label="头像" prop="photo">
+        <Upload :editdata="ruleForm.photo" @upload="selectMobile" />
+      </el-form-item>
       <el-form-item label="姓名" prop="name">
         <el-input v-model="ruleForm.name" placeholder="请输入姓名" />
       </el-form-item>
-      <el-form-item label="手机号" prop="tel">
-        <el-input v-model="ruleForm.tel" placeholder="请输入手机号" />
+      <el-form-item label="手机号" prop="mobile">
+        <el-input v-model="ruleForm.mobile" placeholder="请输入手机号" />
       </el-form-item>
-      <el-form-item label="账号" prop="account">
-        <el-input v-model="ruleForm.account" placeholder="请输入账号" />
+      <el-form-item label="账号" prop="username">
+        <el-input v-model="ruleForm.username" placeholder="请输入账号" />
       </el-form-item>
-      <el-form-item label="密码" prop="pass">
-        <el-input v-model="ruleForm.pass" placeholder="请输入密码" />
+      <el-form-item label="密码" prop="username">
+        <el-input v-model="ruleForm.pwd" placeholder="请输入密码" />
       </el-form-item>
-      <el-form-item label="所属角色" prop="role">
-        <el-select v-model="ruleForm.role" placeholder="请选择">
-          <el-option label="Zone one" value="shanghai" />
-          <el-option label="Zone two" value="beijing" />
+      <el-form-item label="所属角色" prop="username">
+        <el-select
+          v-model="ruleForm.roleIds"
+          clearable
+          multiple
+          placeholder="请选择"
+        >
+          <el-option
+            v-for="item in roleData"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
         </el-select>
       </el-form-item>
     </el-form>
     <template #footer>
       <div class="dialog-footer">
-        <el-button @click="close">取消</el-button>
-        <el-button type="primary" @click="close(true)"> 确定 </el-button>
+        <el-button @click="close(false)">取消</el-button>
+        <el-button type="primary" @click="submit"> 确定 </el-button>
       </div>
     </template>
   </el-dialog>
 </template>
 <script lang="ts" setup>
-import { ref, reactive, defineEmits } from "vue";
+import { defineEmits, reactive, ref } from "vue";
 import type { ComponentSize, FormInstance, FormRules } from "element-plus";
-interface RuleForm {
-  name: string;
-  tel: string;
-  account: string;
-  pass: string;
-  role: string;
-}
+import { AccountAdd } from "@/service/admin/AdminType";
+import Upload from "@/components/upload/AvatarUpload.vue";
+import { RoleList } from "@/service/role/RoleApi";
+import {
+  accountadd,
+  accountget,
+  rolelistForAccount,
+} from "@/service/admin/AdminApi";
+import { accountupdate } from "@/service/account/AccountApi";
 
+const props = defineProps({
+  id: {
+    type: Number,
+    default: 0,
+  },
+});
 const formSize = ref<ComponentSize>("default");
 const ruleFormRef = ref<FormInstance>();
-const ruleForm = reactive<RuleForm>({
+const ruleForm = reactive<AccountAdd>({
+  username: "",
+  pwd: "",
   name: "",
-  tel: "",
-  account: "",
-  pass: "",
-  role: "",
+  photo: "",
+  mobile: "",
+  roleIds: [],
 });
 
-const rules = reactive<FormRules<RuleForm>>({
+const rules = reactive<FormRules<AccountAdd>>({
   name: [{ required: true, message: "姓名不能为空", trigger: "blur" }],
-  tel: [{ required: true, message: "手机号不能为空", trigger: "blur" }],
-
-  account: [{ required: true, message: "账号不能为空", trigger: "blur" }],
-  pass: [{ required: true, message: "密码不能为空", trigger: "blur" }],
-  role: [{ required: true, message: "请选择所属角色", trigger: "blur" }],
+  mobile: [{ required: true, message: "手机号不能为空", trigger: "blur" }],
+  username: [{ required: true, message: "账号不能为空", trigger: "blur" }],
+  pwd: [{ required: true, message: "密码不能为空", trigger: "blur" }],
+  photo: [{ required: true, message: "请选择头像", trigger: "change" }],
+  roleIds: [{ required: true, message: "请选择所属角色", trigger: "change" }],
 });
 
 //弹框
@@ -78,6 +99,53 @@ const emit = defineEmits(["close"]);
 const close = (close: boolean = false) => {
   emit("close", close);
 };
+// 角色列表
+const roleData = ref<any>([]);
+// 获取角色列表
+const getRoleList = async () => {
+  const res: any = await RoleList();
+  if (res?.code === 10000) {
+    roleData.value = res.data.list.map((item: any) => ({
+      label: item.name,
+      value: item.id,
+    }));
+  }
+};
+// 选择头像
+const selectMobile = (val: string) => {
+  ruleForm.photo = val;
+};
+// 提交表单
+const submit = async () => {
+  const isAdd = await ruleFormRef.value?.validate().catch(() => {});
+  if (!isAdd) return false;
+  let res: any;
+  if (props.id) {
+    res = await accountupdate(ruleForm);
+  } else {
+    res = await accountadd(ruleForm);
+  }
+  if (res?.code === 10000) {
+    ElMessage.success(props.id ? "编辑成功" : "新增成功");
+    close(true);
+  } else {
+    ElMessage.error(res.msg);
+  }
+};
+// 数据回显
+const getEdit = async () => {
+  if (!props.id) return false;
+  const res: any = await accountget(props.id);
+  const role: any = await rolelistForAccount(props.id);
+  if (res?.code === 10000 && role?.code === 10000) {
+    Object.assign(ruleForm, res.data);
+    ruleForm.roleIds = role.data.list.map((item: any) => item.id);
+  }
+};
+onMounted(() => {
+  getRoleList();
+  getEdit();
+});
 </script>
 <style lang="less" scoped>
 .el-input {
---
+++ src/views/system/AccountView.vue
@@ -0,0 +1,117 @@
+<template>
+  <!-- 账号管理 -->
+  <el-card style="max-width: 100%">
+    <el-button type="primary" @click="isdialog = true">新增</el-button>
+    <ManagementDialog v-if="isdialog" :id="deitID" @close="close" />
+    <!-- 表格 -->
+    <MayTable :tableData="data.tableData" :tableItem="data.tableItem">
+      <template #operate="{ data }">
+        <el-button text type="primary" @click="edit(data.id)">编辑</el-button>
+        <el-button text type="primary" @click="del(data.id)">删除</el-button>
+      </template>
+    </MayTable>
+    <Pagination :total="total" @page="getpage" @psize="getpsize" />
+  </el-card>
+</template>
+
+<script lang="ts" setup>
+import { defineAsyncComponent, onMounted, reactive, ref } from "vue";
+import ManagementDialog from "@/components/dialog/system/ManagementDialog.vue";
+import { getMessageBox } from "@/utils/utils";
+import { ElMessage } from "element-plus";
+import { accountdelete, adminList } from "@/service/admin/AdminApi"; // import ManagementView from "@/database/ManagementView.json";
+// import ManagementView from "@/database/ManagementView.json";
+const MayTable = defineAsyncComponent(
+  () => import("@/components/table/MayTable.vue")
+);
+const Pagination = defineAsyncComponent(
+  () => import("@/components/pagination/MayPagination.vue")
+);
+const data = reactive({
+  tableData: [] as any,
+  tableItem: [
+    {
+      prop: "id",
+      label: "序号",
+    },
+    {
+      prop: "name",
+      label: "姓名",
+    },
+    {
+      prop: "mobile",
+      label: "手机号",
+    },
+    {
+      prop: "username",
+      label: "账号",
+    },
+    {
+      prop: "pwd",
+      label: "密码",
+    },
+  ],
+});
+//弹出框
+const isdialog = ref(false);
+const close = (isclose: boolean) => {
+  if (isclose) {
+    getlist();
+  }
+  if (deitID.value) {
+    deitID.value = 0;
+  }
+  isdialog.value = false;
+};
+// 编辑
+// 账号id
+const deitID = ref(0);
+const edit = (id: number) => {
+  console.log(id);
+  deitID.value = id;
+  isdialog.value = true;
+};
+// 删除
+const del = async (id: number) => {
+  let res = await getMessageBox("是否确认删除该角色", "删除后将不可恢复");
+  console.log(1111, res);
+  if (!res) return ElMessage.info("取消删除");
+  const del: any = await accountdelete(id);
+  if (del?.code === 10000) {
+    ElMessage.success("删除成功");
+    await getlist();
+  }
+};
+//账号列表数据
+const total = ref(0);
+const params = reactive({
+  page: 1,
+  pageSize: 5,
+});
+// 获取账号列表数据
+const getlist = async () => {
+  const res: any = await adminList(params);
+  if (res?.code === 10000) {
+    total.value = res.data.counts;
+    data.tableData = res.data.list;
+  }
+};
+//分页
+const getpage = (val: number) => {
+  params.page = val;
+  getlist();
+};
+const getpsize = (val: number) => {
+  params.pageSize = val;
+  getlist();
+};
+onMounted(() => {
+  getlist();
+});
+</script>
+
+<style lang="less" scoped>
+.el-button {
+  margin-bottom: 20px;
+}
+</style>
